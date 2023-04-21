f091bfd589153e0ab7d2a551f64ea7bbfcd52b5b
Bohdan Pashkovskyi <bpashkovskyi@gmail.com>
2023-04-21 14:22:45 +0300

    Add project files.

---
+++ DbContextTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OwnedEntitiesTest;
+
+[TestClass]
+public class DbContextTests
+{
+    [AssemblyInitialize]
+    public static async Task AssemblyInitialize(TestContext testContext)
+    {
+        var context = new MyDbContext();
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+    }
+
+    [AssemblyCleanup]
+    public static async Task AssemblyCleanup()
+    {
+        var context = new MyDbContext();
+        await context.Database.EnsureDeletedAsync();
+    }
+
+    [TestMethod]
+    public async Task UpdateParentEntityShouldSucceed()
+    {
+        var context = new MyDbContext();
+
+        var parentEntity = new ParentEntity
+        {
+            Name = "Parent Entity",
+            OwnedEntities = new[]
+            {
+                new OwnedEntity
+                {
+                    Name = "Owned Entity 1",
+                },
+                new OwnedEntity
+                {
+                    Name = "Owned Entity 2"
+                }
+            }
+        };
+
+        await context.AddAsync(parentEntity);
+        await context.SaveChangesAsync();
+
+        // Reset context
+        context = new MyDbContext();
+
+        var parentEntityToModify = await context.ParentEntities.AsNoTracking().FirstAsync(e => e.Name == "Parent Entity");
+
+        parentEntityToModify.Name = "Parent Entity Modified";
+        parentEntityToModify.OwnedEntities.Add(new OwnedEntity
+        {
+            Name = "Owned Entity 3"
+        });
+
+        context.Update(parentEntityToModify);
+        await context.SaveChangesAsync();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OwnedEntitiesTest
+{
+    public class ParentEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<OwnedEntity> OwnedEntities { get; set; }
+    }
+
+    public class OwnedEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        // Uncomment this Id to make test passed
+        ////public Guid ParentEntityId { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<ParentEntity> ParentEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ParentEntity>()
+                .OwnsMany(parent => parent.OwnedEntities);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFProviders.InMemory;Trusted_Connection=True;ConnectRetryCount=0");
+        }
+    }
+}
